dd928ee078a2423dda72df43e92f0e9930476d56
Jon Evans <jle@foraker.com>
2015-12-07 21:12:57 -0700

    Set document title once site retrieved

--- app/routes/index.js
+++ app/routes/index.js
@@ -3,6 +3,10 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   currentSite: Ember.inject.service(),
 
+  afterModel: function(site) {
+    document.title = site.get('title')
+  },
+
   model() {
     return this.get('currentSite').site();
   }

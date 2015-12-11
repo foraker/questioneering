1cc9053cb47a7a080f6bc277818fd306d3f9d83e
Jeff and Jon <jdc+jle@foraker.com>
2015-12-11 13:40:06 -0700

    Disallow direct access to questions/resolutions before Site is loaded

--- app/routes/application.js
+++ app/routes/application.js
@@ -3,6 +3,12 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   nextStepCalculator: Ember.inject.service(),
 
+  afterModel: function() {
+    if (this.get('store').peekAll('site').get('length') == 0) {
+      return this.transitionTo('index');
+    }
+  },
+
   actions: {
     proceed(question) {
       this.get('nextStepCalculator').getStepAfter(question).then(function(nextStep) {

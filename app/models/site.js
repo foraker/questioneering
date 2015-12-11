a962a6abac491657e7b3fec3d365a5f31f813ccd
Jon Evans <jle@foraker.com>
2015-12-11 11:40:13 -0700

    Set meta description and keywords from site

--- app/models/site.js
+++ app/models/site.js
@@ -3,6 +3,8 @@ import DS from 'ember-data';
 export default DS.Model.extend({
   title: DS.attr('string'),
   content: DS.attr('string'),
+  description: DS.attr('string'),
+  keywords: DS.attr('string'),
   questions: DS.hasMany(),
   resolutions: DS.hasMany()
 });
